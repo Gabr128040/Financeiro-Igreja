63185aa1afa486951ef35a2492974f00750096b0
Gabr128040 <gabr.128040@gmail.com>
2025-03-14 12:59:08 -0300

    Inicial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Igreja Financeiro</h1>
+        <p>Gerencie dízimos, ofertas, gastos, contas e contribuições.</p>
+      </header>
+      <main>
+        {/* Aqui serão adicionados os componentes de CRUD */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
